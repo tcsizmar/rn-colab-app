6342e596b466cddc0910d6d0c2eedd40846c3605
Nickolas dos Santos Vieira <nickolas.vieira@hotmail.com>
2021-03-27 18:59:32 -0300

    Começando Splash

--- src/views/splash/index.js
+++ src/views/splash/index.js
@@ -1,17 +1,32 @@
+/** Bibliotecas */
 import React from 'react'
-import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme } from 'react-native'
+import { ImageBackground, SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View } from 'react-native'
+/** Serviços */
 
-const App = () => {
+/** Componentes */
+
+
+/** Outros Imports */
+const splash_img = { uri: 'https://cdn.pixabay.com/photo/2017/01/18/17/52/calendar-1990453_960_720.jpg' }
+
+const Splash = () => {
   const isDarkMode = useColorScheme() === 'dark'
 
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView contentInsetAdjustmentBehavior='automatic'>
-        <Text>ESTA É A TELA SPLASH</Text>
-      </ScrollView>
+      <View style={{ flex: 1}}>
+        {/* <ImageBackground
+          source={splash_img}
+          style={{ flex: 1, resizeMode: 'cover' , width: '100%', height:'100%'}}
+        /> */}
+
+
+        {/* </ImageBackground> */}
+        <Text>Nickolas</Text>
+      </View>
     </SafeAreaView>
   )
 }
 
-export default App
+export default Splash
