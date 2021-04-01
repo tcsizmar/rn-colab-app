27e4abd41989c551e4322ff7d6704c84d0bd33be
Thiago Csizmar <tcsizmar@gmail.com>
2021-03-31 21:41:54 -0300

    Lendo os dados do async storage para salvar na lista local de exibição

--- src/views/splash/index.js
+++ src/views/splash/index.js
@@ -1,21 +1,37 @@
 /** Bibliotecas */
-import React, { useEffect } from 'react'
-import { ImageBackground, SafeAreaView, ScrollView, Text, View } from 'react-native'
+import React, { useContext, useEffect } from 'react'
+import { ImageBackground, Text, View } from 'react-native'
 /** Serviços */
+import { AppContext } from '_services'
+import { StorageHelper } from '_utils'
 
 /** Componentes */
 
 /** Outros Imports */
 import Styles from '_views/splash/styles'
+import { STORAGE_LIST } from '../../../src/config'
 const splash_img = require('_assets/images/calendar-splash.jpg')
 
 const Splash = ({ navigation}) => {
+  const context = useContext(AppContext)
 
   useEffect(()=> {
+    getData()   
+
+    // SO PASSA PRA PROXIMA TELA DEPOIS DE 3 SEGUNDOS
     setTimeout(() => {
       navigation.navigate('Main')
     }, 3000)
   })
+
+  // PEGANDO OS DADOS DO APLICATIVO QUE ESTAO SALVOS NO ASYNC STORAGE
+  const getData = async () => {
+    await StorageHelper.clearStorage()
+    let objetoComArrayAgenda = await StorageHelper.retrieveData(STORAGE_LIST)
+    objetoComArrayAgenda = objetoComArrayAgenda ? JSON.parse(objetoComArrayAgenda) : []
+
+    context.setAgenda(objetoComArrayAgenda.agendaStorage)
+  }
 
   return (
     <View style={Styles.container}>
